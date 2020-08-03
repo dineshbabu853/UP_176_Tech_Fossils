3ce6f6d5f9eac2bc7d80f4b44eb3aa04d622c0a1
dinesh babu <30918023+dineshbabu853@users.noreply.github.com>
2020-08-03 15:02:59 +0530

    Added the LDA algorithm for Topic Modeling the manual

---
+++ comprehend.py
@@ -0,0 +1,96 @@
+import boto3
+import os
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords 
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+
+def lemmatize_stemming(text):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+# Tokenize and lemmatize
+def preprocess(text):
+    result=[]
+    for token in gensim.utils.simple_preprocess(text) :
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+
+
+def create_intent(project_id, display_name, training_phrases_parts,
+                  message_texts):
+    """Create an intent of the given intent type."""
+    import dialogflow_v2 as dialogflow
+    intents_client = dialogflow.IntentsClient()
+
+    parent = intents_client.project_agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.types.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.types.Intent.Message.Text(text=message_texts)
+    message = dialogflow.types.Intent.Message(text=text)
+
+    intent = dialogflow.types.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message])
+
+    response = intents_client.create_intent(parent, intent)
+
+    print('Intent created: {}'.format(response))
+
+
+# Document
+documentName = "dinesh.jpg"
+
+# Read document content
+with open(documentName, 'rb') as document:
+    imageBytes = bytearray(document.read())
+
+access_key_id = os.getenv('MY_ACCESS_KEY_ID')
+secret_access_key = os.getenv('MY_SECRET_ACCESS_KEY')
+
+# Amazon Textract client
+textract = boto3.client('textract', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+
+# Call Amazon Textract
+response = textract.detect_document_text(Document={'Bytes': imageBytes})
+
+#print(response)
+
+text=""
+# Print detected text
+for item in response["Blocks"]:
+    if item["BlockType"] == "LINE":
+        text = text + " " + item["Text"]
+        
+
+
+# compile documents
+doc_complete = text.split('.');
+
+print('\n\nData\n\n')
+print(doc_complete)
+
+# set of stopwords
+stop = set(stopwords.words('english'))
+exclude = set(string.punctuation) 
+lemma = WordNetLemmatizer()
+
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    return normalized
+
+doc_clean = [clean(doc).split() for doc in doc_complete]    
+print('\n\nCleaned Data\n\n')
+print(doc_clean)
+create_intent("test-agent-nocirr","manual",doc_clean,text)
+
+
